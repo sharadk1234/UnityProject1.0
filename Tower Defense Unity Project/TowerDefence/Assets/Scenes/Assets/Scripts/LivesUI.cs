96d625b965ef040feaa9630bb5b365c6db5cd33b
BlackholeCoding <46094697+sharadk1234@users.noreply.github.com>
2020-03-04 14:24:52 -0800

    Tower Defence Game in Unity
    Tower Defence Game in Unity

---
+++ Tower Defense Unity Project/TowerDefence/Assets/Scenes/Assets/Scripts/LivesUI.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LivesUI : MonoBehaviour {
+
+	public Text livesText;
+	// Update is called once per frame
+	void Update () {
+		livesText.text = PlayerStats.Lives.ToString() + " LIVES";
+	}
+}
